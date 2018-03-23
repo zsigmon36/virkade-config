3018350b7a31fb25a8472f38d57231849797a356
Zachary Sigmon <zachary.sigmon@hp.com>
2018-03-23 14:12:35 -0500

    database commands for en_US and utf 8

--- virkade-db-create.sh
+++ virkade-db-create.sh
@@ -1,10 +1,16 @@
 apt-get install postgresql-9.6;
 
 su root -p admin
+nano /etc/locale.gen (enable en_US.utf8)
+locale-gen
+nano /etc/postgresql/9.6/main/postgresql.conf (set the encoding)
+/etc/init.d/postgresql restart
 su postgres -p sigbus36
 initdb -D /opt/app/virkade/postgresdb/data
 CREATE USER virkade WITH PASSWORD 'Reid.Zac36';
 alter role virkade with createDB
 alter role virkade with login
-su virkade -p virkade
-CREATE DATABASE virkadecms;
+su virkade -p 'Reid.Zac36'
+createdb -E UTF8 --lc-collate='en_US.utf8' --lc-ctype='en_US.utf8' virkadeCms --template template0
+psql virkadeCms
+create schema virkade;
