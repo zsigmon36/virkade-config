1b58a4bb24ba3b2e1a9a70864a2cc2ba713fdfe3
zachary.sigmon <sigmonbus36@gmail.com>
2020-05-11 22:48:31 -0500

    adding some raid config hints

--- virkade-db-create.sh
+++ virkade-db-create.sh
@@ -1,3 +1,15 @@
+-- DNS bind9
+/etc/bind
+
+
+
+--create raid
+mdadm --create /dev/md0 --level=raid5 --raid-devices=3 /dev/sda1 /dev/sdb1 /dev/sdc1
+mdadm --detail /dev/md0
+mdadm --manage /dev/md0 --add /dev/sdc1
+mdadm --detail --scan --verbose | tee -a /etc/mdadm/mdadm.conf
+mkfs.ext4 /dev/md0
+
 --Disk mounting
 lsblk
 sudo mount /dev/md0 /opt/app/virkade
@@ -13,9 +25,10 @@ nano /etc/postgresql/9.6/main/postgresql.conf (set the encoding)
 /etc/init.d/postgresql restart
 su postgres -p sigbus36
 initdb -D /opt/app/virkade/postgresdb/data
+psql
 CREATE USER virkade WITH PASSWORD 'Reid.Zac36';
-alter role virkade with createDB
-alter role virkade with login
+alter role virkade with createDB;
+alter role virkade with login;
 su virkade -p 'Reid.Zac36'
 createdb -E UTF8 --lc-collate='en_US.utf8' --lc-ctype='en_US.utf8' virkadeCms --template template0
 psql virkadeCms
